5d76f60b254f30cb5dcbe7591ee4c95ac78b4af7
Ignacio Capodanno <icapodanno@gmail.com>
2020-09-20 12:13:23 -0300

    Addinf flip effect over card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import logo from './person.svg';
 import flag from './swiss.png';
 import './App.css';
-import {motion, useCycle, useMotionValue, useTransform} from "framer-motion";
+import {motion} from "framer-motion";
 import {Frame, Stack} from "framer";
 
 export const App = () => {
-  const y = useMotionValue(40);
-  const x = useTransform(y, value => value * 2)
+
+  const leftCol = [{att: 'PAC', value: 51}, {att: 'SHO', value: 72}, {att: 'PAS', value: 82}];
+  const rightCol = [{att: 'DRI', value: 72}, {att: 'DEF', value: 74}, {att: 'PHY', value: 78}];
 
   return (
       <div className="App">
         <header className="App-header">
+          <div className={'flip-box'}>
+          <div className={'flip-box-inner'}>
           <Frame
+              className={'flip-box-back'}
               width={"350px"}
               height={"450px"}
               background={"#EEDE88"}
               radius={30}
+              rotateY={180}
           >
             <Stack
                 backgroundColor={"transparent"}
@@ -26,20 +31,22 @@ export const App = () => {
             >
               <span style={{fontSize: '72px', color: '#47390E', marginBottom: '-10px'}}>81</span>
               <motion.div style={{fontSize: '36px', color: '#47390E'}}>CM</motion.div>
-              <motion.div animate={{ rotate: 180 }}
-                          transition={{ duration: 8, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }} ><motion.img src={flag} height={'40px'}></motion.img></motion.div>
+              <motion.div animate={{rotate: 180}}
+                          transition={{duration: 8, ease: "easeInOut", repeat: Infinity, repeatType: "reverse"}}>
+                <motion.img src={flag} height={'40px'}></motion.img>
+              </motion.div>
             </Stack>
             <motion.div
-                transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }}
-                animate={{ y: [0, -20] }}
+                transition={{duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse"}}
+                animate={{y: [0, -20]}}
             >
-            <Frame
-                image={logo}
-                scale={0.9}
-                right={30}
-                top={30}
-                backgroundColor={"transparent"}
-            /></motion.div>
+              <Frame
+                  image={logo}
+                  scale={0.9}
+                  right={30}
+                  top={30}
+                  backgroundColor={"transparent"}
+              /></motion.div>
             <Frame center top={'75%'} backgroundColor={"transparent"}>
               <span style={{fontSize: '48px', color: '#47390E'}}>XHAKA</span>
               <Stack
@@ -48,9 +55,12 @@ export const App = () => {
                   top={"35%"}
                   left={"15%"}
               >
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>51</span> PAC</motion.div>
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>72</span> SHO</motion.div>
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>82</span> PAS</motion.div>
+                {leftCol.map(data =>
+                    <motion.div whileHover={{scale: 1.2}} style={{fontSize: '24px', color: '#47390E'}}><span
+                        style={{fontWeight: 'bold'}}>{data.value}</span> {data.att}
+                    </motion.div>
+                )
+                }
               </Stack>
               <Stack
                   backgroundColor={"transparent"}
@@ -58,14 +68,26 @@ export const App = () => {
                   top={"35%"}
                   left={"85%"}
               >
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>72</span> DRI</motion.div>
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>74</span> DEF</motion.div>
-                <motion.div whileHover={{ scale: 1.2 }} style={{fontSize: '24px', color: '#47390E'}}><span style={{fontWeight:'bold'}}>78 </span>PHY</motion.div>
+                {rightCol.map(data =>
+                    <motion.div whileHover={{scale: 1.2}} style={{fontSize: '24px', color: '#47390E'}}><span
+                        style={{fontWeight: 'bold'}}>{data.value}</span> {data.att}
+                    </motion.div>
+                )
+                }
               </Stack>
             </Frame>
-
-
           </Frame>
+          <Frame
+              className={'flip-box-front'}
+              width={"350px"}
+              height={"450px"}
+              background={"#EEDE88"}
+              radius={30}
+          >
+            FIFA CARD
+          </Frame>
+          </div>
+          </div>
         </header>
       </div>
   );
